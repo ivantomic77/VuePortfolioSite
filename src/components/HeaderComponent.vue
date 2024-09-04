e88f1d2d55eb8c07cf9500013255fb811285a0ff
ivantomic77 <ivan.tomic2108@gmail.com>
2024-09-04 20:12:04 +0200

    Added events

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,14 +1,19 @@
-<script lang="ts">
+<script setup lang="ts">
 import {
     IconLinkedinWithCircle,
     IconGithubWithCircle,
 } from "@iconify-prerendered/vue-entypo-social";
+import { inject } from "vue";
 
-export default {
-    components: {
-        IconLinkedinWithCircle,
-        IconGithubWithCircle,
-    },
+
+const saEvent = inject<any>("saEvent");
+
+const visitGithub = () => {
+    saEvent('github_visit'); 
+};
+
+const visitLinkedIn = () => {
+    saEvent('linkedin_visit');
 };
 </script>
 
@@ -16,10 +21,10 @@ export default {
     <div class="flex justify-between">
         <a href="#" aria-label="Go to the top."><img alt="Ivan Tomic logo" src="../assets/logo.png" class="h-12 w-12" /></a>
         <div class="flex gap-2">
-            <a href="https://www.linkedin.com/in/itomic7/" aria-label="Visit my LinkedIn page." target="_blank"
+            <a href="https://www.linkedin.com/in/itomic7/" v-on:click="visitLinkedIn" aria-label="Visit my LinkedIn page." target="_blank"
                 ><IconLinkedinWithCircle class="h-9 w-9 hover:animate-spin"
             /></a>
-            <a href="https://github.com/ivantomic77" aria-label="Visit my Github page." target="_blank"
+            <a href="https://github.com/ivantomic77" v-on:click="visitGithub" aria-label="Visit my Github page." target="_blank"
                 ><IconGithubWithCircle class="h-9 w-9 hover:animate-spin"
             /></a>
         </div>
