b1192d3b5d02ffada5f8e380d038ef2a8cc37acb
ivantomic77 <ivan.tomic2108@gmail.com>
2024-09-04 20:25:03 +0200

    refactor: removed custom analytics events

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,21 +3,12 @@ import {
     IconLinkedinWithCircle,
     IconGithubWithCircle,
 } from "@iconify-prerendered/vue-entypo-social";
-import { inject, ref, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 
-const saEvent = inject<any>("saEvent");
 const year = ref(0);
 
 const getYear = () => {
     year.value = new Date().getFullYear();
-};
-
-const visitGithub = () => {
-    saEvent('github_visit');
-};
-
-const visitLinkedIn = () => {
-    saEvent('linkedin_visit');
 };
 
 onMounted(() => {
@@ -31,10 +22,10 @@ onMounted(() => {
         <div class="flex flex-col gap-3">
             <img  class="w-80" alt="Email address" src="../assets/email.png" width="320px" height="48px" />
             <div class="flex justify-center items-center gap-2">
-                <a href="https://www.linkedin.com/in/itomic7/" v-on:click="visitLinkedIn" aria-label="Visit my LinkedIn page." target="_blank"
+                <a href="https://www.linkedin.com/in/itomic7/" aria-label="Visit my LinkedIn page." target="_blank"
                     ><IconLinkedinWithCircle class="h-9 w-9 hover:animate-spin"
                 /></a>
-                <a href="https://github.com/ivantomic77" v-on:click="visitGithub" aria-label="Visit my Github page." target="_blank"
+                <a href="https://github.com/ivantomic77" aria-label="Visit my Github page." target="_blank"
                     ><IconGithubWithCircle class="h-9 w-9 hover:animate-spin"
                 /></a>
             </div>
