003706afda6c82336eb2aab5bffcb7849643ac2f
ivantomic77 <ivan.tomic2108@gmail.com>
2024-11-01 20:23:57 +0100

    #4: fix overflow

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import CustomCursor from "./components/CustomCursor.vue";
 
 <template>
     <div
-        class="bg-white w-screen sm:w-[80vw] rounded-lg p-4 max-w-6xl shadow-lg flex flex-col gap-24">
+        class="bg-white w-full sm:w-[80vw] rounded-lg p-4 max-w-6xl shadow-lg flex flex-col gap-24">
         <header-component />
         <router-view />
         <Footer />
