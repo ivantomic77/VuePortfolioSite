da57ba9af85b72caf23da447e75c4dedf45bad46
Ivan Tomić <61111730+ivantomic77@users.noreply.github.com>
2025-06-06 20:38:33 +0200

    fix: Fixed broken html structure and minor layout adjustments (#6)

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -9,7 +9,7 @@ export default {
                     role: "Software Developer",
                     url: "https://www.og-cs.hr",
                     period: "Jul 2022 - Aug 2022",
-                    description: "As a high school intern, I gained skills in full-stack development using Spring Boot, Angular and Docker containerization"
+                    description: "Worked on a full-stack web application using Spring Boot for backend APIs and Angular for the frontend. Learned how to containerize apps with Docker and got familiar with dev workflows."
                 },
                 {
                     companyName: "OG Consultancy Services",
@@ -17,7 +17,7 @@ export default {
                     role: "Backend Developer",
                     url: "https://www.og-cs.hr",
                     period: "Jul 2023 - Present",
-                    description: "I collaborate with clients to create innovative solutions, primarily focusing on Spring Boot while continuously to expanding my tech horizons"
+                    description: "I develop backend systems using Java and Spring Boot, design solutions, build REST APIs, and manage data with PostgreSQL and other databases. I work closely with frontend developers and DevOps team to automate testing and deployment. Since the projects vary, I’m always learning new tools and technologies as needed."
                 }
             ],
             colorCache: {}
@@ -34,32 +34,35 @@ export default {
 
             return color;
         }
-  }
+    }
 }
 </script>
 
 <template>
     <section class="sm:mt-36 mt-32 md:mx-16 mx-6">
-        <h1 class="text-4xl font-semibold">Experience</h1>
-        <h2 class="text-xl mb-14">(a tale of bugs, bytes, and brilliant breakthroughs)</h2>
+        <h2 class="text-4xl font-semibold">Experience</h2>
+        <p class="text-xl mb-14">(a tale of bugs, bytes, and brilliant breakthroughs)</p>
 
-        <div
-            class="mt-10">
-            <div class="flex flex-wrap sm:gap-8 gap-8">
-                <div v-for="experience in experiences" class="flex flex-col justify-between shadow-md border-t-4 rounded-lg max-w-sm" :style="{ borderColor: generateRandomColor() }">
+        <div class="mt-10">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                <div v-for="experience in experiences"
+                    class="flex flex-col justify-between shadow-md border-t-4 rounded-lg w-full"
+                    :style="{ borderColor: generateRandomColor() }">
                     <div class="px-4 py-5">
-                        <h2 class="text-sm">{{ experience.period }}</h2>
-                        <h2 class="text-lg font-semibold">{{ experience.companyName }}</h2>
+                        <p class="text-sm">{{ experience.period }}</p>
+                        <h3 class="text-lg font-semibold">{{ experience.companyName }}</h3>
                         <div class="flex gap-2 text-sm">
-                            <h2>{{ experience.employmentType }}</h2>
-                            <h2>&#8226;</h2>
-                            <h2>{{ experience.role }}</h2>
+                            <p>{{ experience.employmentType }}</p>
+                            <span>&#8226;</span>
+                            <p>{{ experience.role }}</p>
                         </div>
                         <div class="mt-4">{{ experience.description }}</div>
                     </div>
-                    <a :href="experience.url" class="bg-black text-white p-2 rounded-b-lg flex justify-between pr-4 transition ease-in-out hover:bg-opacity-70" target="_blank">
-                        <h2>Go to the site</h2>
-                        <h2 class="font-bold">></h2>
+                    <a :href="experience.url"
+                        class="bg-black text-white p-2 rounded-b-lg flex justify-between pr-4 transition ease-in-out hover:bg-opacity-70"
+                        target="_blank">
+                        <span>Go to the site</span>
+                        <span class="font-bold">></span>
                     </a>
                 </div>
             </div>
--- src/components/TechStack.vue
+++ src/components/TechStack.vue
@@ -50,12 +50,12 @@ export default {
                         icon: "logos:linode",
                     },
                     {
-                        name: "Kubernetes",
-                        icon: "skill-icons:kubernetes",
-                    },
-                    {
                         name: "Docker",
                         icon: "skill-icons:docker",
+                    },
+                    {
+                        name: "Kubernetes",
+                        icon: "skill-icons:kubernetes",
                     },
                     {
                         name: "Git",
@@ -93,20 +93,19 @@ export default {
 
 <template>
     <section class="sm:mt-60 mt-28 md:mx-16 mx-6">
-        <h1 class="text-4xl font-semibold">Technologies I've used...</h1>
-        <h2 class="text-xl mb-14">(going beyond main tech stack)</h2>
+        <h2 class="text-4xl font-semibold">Technologies I've used...</h2>
+        <p class="text-xl mb-14">(going beyond main tech stack)</p>
 
-        <div
-            class="mt-10">
-            <div class="flex flex-wrap sm:gap-10 gap-10">
+        <div class="mt-10">
+            <div class="grid grid-cols-10 sm:gap-10 gap-10">
                 <span
                     v-for="technology in tech"
                     :key="technology.name"
-                    class="flex flex-col items-center w-[45px]">
+                    class="flex flex-col items-center">
                     <Icon
                         :icon="technology.icon"
-                        class="sm:text-6xl text-5xl" />
-                    <h1 class="text-sm whitespace-nowrap">{{ technology.name }}</h1>
+                        class="sm:text-6xl text-5xl block" />
+                    <p class="text-sm whitespace-nowrap">{{ technology.name }}</p>
                 </span>
             </div>
         </div>
